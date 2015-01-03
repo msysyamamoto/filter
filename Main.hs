343d8cc3cd72d713599b65bd4a0e517ef1cc5f25
ymmtmsys <m2yamamoto@gmail.com>
2015-01-03 19:40:55 +0900

    command line option

--- Main.hs
+++ Main.hs
@@ -1,13 +1,11 @@
+import Data.Monoid
+import           Options.Applicative
 import System.Environment (getArgs, getProgName)
 import Control.Applicative ((<$>))
 import Tools.Filter
 
 main :: IO ()
 main = do
-    pgname <- getProgName
-    args   <- parseArgs <$> getArgs
-    case args of
-        Left err             -> printUsage pgname err
-        Right (file1, file2) -> do
-            filtered <- filterNotMatching file1 file2
-            mapM_ putStrLn filtered
+    opts     <- execParser (info optionP mempty)
+    filtered <- filterNotMatching opts
+    mapM_ putStrLn filtered
--- Tools/Filter.hs
+++ Tools/Filter.hs
@@ -6,8 +6,50 @@ import           Data.Set            (Set, fromList, notMember)
 import           Data.ByteString     (ByteString)
 import qualified Data.ByteString.Char8 as B
 
-filterNotMatching :: FilePath -> FilePath -> IO [String]
-filterNotMatching f1 f2 = do
+import           Data.Monoid
+import           Options.Applicative
+
+data Option = Option
+    { input   :: FileProp
+    , pattern :: FileProp
+    } deriving (Show)
+
+data FileProp = FileProp
+    { positions :: Int
+    , delimiter :: ByteString
+    , path      :: FilePath
+    } deriving (Show)
+
+positionsP :: String -> Parser Int
+positionsP lavel = option auto
+    (  long lavel
+    <> metavar "POSITION"
+    <> help "The list specifies fields"
+    <> value 0
+    <> showDefault
+    )
+
+filePropP :: String -> Parser FileProp
+filePropP lavel = fp <$> positionsP lavel
+  where
+    fp pos = FileProp pos "\t" ""
+
+fileP :: String -> Parser FilePath
+fileP label = strArgument
+    (  metavar label
+    <> help "file path"
+    )
+
+optionP :: Parser Option
+optionP = (<*>) helper $ opts <$> filePropP "inpos" <*> filePropP "patpos" <*> fileP "filepath" <*> fileP "filepath2"
+  where
+    opts fp1 fp2 f1 f2 = Option fp1' fp2'
+      where
+        fp1' = fp1 { path = f1 }
+        fp2' = fp2 { path = f2 }
+
+filterNotMatching :: Option -> IO [String]
+filterNotMatching (Option (FileProp _ _ f1) (FileProp _ _ f2)) = do
     ls   <- map (B.split '\t') <$> readLines f1
     keys <- fromList <$> readLines f2
     return . map (B.unpack . join "\t") $ runFilter ls keys
@@ -28,13 +70,3 @@ join glue = go
     go []     = ""
     go [x]    = x
     go (x:xs) = B.append (B.append x glue) $ go xs
-
-printUsage :: String -> String -> IO ()
-printUsage pgname err = do
-    putStrLn $ "Error: " ++ err
-    putStrLn $ "Usage: " ++ pgname ++ " file1 file2"
-
-parseArgs :: [String] -> Either String (FilePath, FilePath)
-parseArgs args
-    | length args < 2  = Left "missing argument"
-    | otherwise = Right (args !! 0, args !! 1)
